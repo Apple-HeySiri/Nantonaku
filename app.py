2e963b7f4c9a30c1e4a4b8365a806ef1e7cbd2f3
Siri <heysiri.assistant@gmail.com>
2024-11-07 12:26:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import fastapi
 
 app = fastapi.FastAPI()
 
-@app.get("/")
-def read_root():
-    return Hello
+@app.get("/", response_class=fastapi.responses.HTMLResponse)
+async def main():
+    return """
+    <html>
+        <head>
+            <title>Test</title>
+        </head>
+        <body>
+            <h1>Hello world!</h1>
+        </body>
+    </html>
+    """
